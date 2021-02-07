d4e568ca215e213e4dbee04eeedd785c85a5c07b
Rickey Dong <rickeyd2004@gmail.com>
2021-02-06 21:02:38 -0500

    tested reverse method and it works

---
+++ 01-recursion/RecursionTester.java
@@ -0,0 +1,11 @@
+public class RecursionTester
+{
+  public static void main(String[] args)
+  {
+    System.out.println(Recursion.reverse("hello"));
+    System.out.println(Recursion.reverse(""));
+    System.out.println(Recursion.reverse("h"));
+    System.out.println(Recursion.reverse("hi"));
+    System.out.println(Recursion.reverse("hey"));
+  }
+}
