9fd1a133b2a318cc023210a772b5024a43f2442b
Rickey Dong <rickeyd2004@gmail.com>
2021-02-06 21:27:18 -0500

    created countNDLW method

--- 01-recursion/Recursion.java
+++ 01-recursion/Recursion.java
@@ -11,4 +11,23 @@ public class Recursion
       return s.charAt(s.length()-1) + reverse(s.substring(0,s.length()-1));
     }
   }
+  public static long countNoDoubleLetterWords(int length, String word)
+  {
+    if (length == 0)
+    {
+      return 1;
+    }
+    else
+    {
+      long count = 0;
+      for (char letter = 'a'; letter <= 'z'; letter++)
+      {
+        if (word.length() == 0 || word.charAt(word.length()-1) != letter)
+        {
+          count += countNoDoubleLetterWords(length-1, word+letter);
+        }
+      }
+      return count;
+    }
+  }
 }
