9bf90e2f4794772c19679552adfa2dcc8527b278
Rickey Dong <rickeyd2004@gmail.com>
2021-02-06 21:31:18 -0500

    tested countNDLW method and it works

--- 01-recursion/RecursionTester.java
+++ 01-recursion/RecursionTester.java
@@ -7,5 +7,10 @@ public class RecursionTester
     System.out.println(Recursion.reverse("h"));
     System.out.println(Recursion.reverse("hi"));
     System.out.println(Recursion.reverse("hey"));
+    System.out.println("=========================");
+    System.out.println(Recursion.countNoDoubleLetterWords(0,"") + " should be 1");
+    System.out.println(Recursion.countNoDoubleLetterWords(1,"") + " should be 26");
+    System.out.println(Recursion.countNoDoubleLetterWords(2,"") + " should be 650");
+    System.out.println(Recursion.countNoDoubleLetterWords(3,""));
   }
 }
