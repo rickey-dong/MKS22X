4db163639ad605e59599b914176b221a8bec9b19
Rickey Dong <rickeyd2004@gmail.com>
2021-02-21 15:48:34 -0500

    fixed constructor

--- 02-queens/QueenBoard.java
+++ 02-queens/QueenBoard.java
@@ -4,5 +4,16 @@ public class QueenBoard
   public QueenBoard(int size)
   {
     board = new int[size][size];
+    for (int row = 0; row < size; row++)
+    {
+      for (int col = 0; col < size; col++)
+      {
+        board[row][col] = 0;
+      }
+    }
+  }
+  private boolean addQueen(int row, int col)              // abc
+  {                                                       // def          row 0 is abc, col 0 is ad
+    
   }
 }
