f0f3bf0e437b500c9b4c84a688080e788b62dbe9
Rickey Dong <rickeyd2004@gmail.com>
2021-02-21 17:02:18 -0500

    tested queen board so far, constructor, add, remove, tostring work

--- 02-queens/QueenBoardTester.java
+++ 02-queens/QueenBoardTester.java
@@ -2,6 +2,24 @@ public class QueenBoardTester
 {
   public static void main(String[] args)
   {
-    
+    QueenBoard FourByFour = new QueenBoard(4);
+    System.out.println(FourByFour);
+    System.out.println(FourByFour.addQueen(0,0) + " should be true");
+    System.out.println(FourByFour);
+    System.out.println(FourByFour.addQueen(0,0) + " should be false bc it occupied");
+    System.out.println(FourByFour);
+    System.out.println(FourByFour.addQueen(0,1) + " should be false bc it threatened");
+    System.out.println(FourByFour);
+    System.out.println(FourByFour.addQueen(1,1) + " should be false bc it threatened");
+    FourByFour.removeQueen(0,0);
+    System.out.println(FourByFour);
+    System.out.println(FourByFour.addQueen(0,1) + " should be true");
+    System.out.println(FourByFour);
+    System.out.println(FourByFour.addQueen(2,2) + " should be true");
+    System.out.println(FourByFour);
+    System.out.println(FourByFour.addQueen(3,3) + " should be false");
+    System.out.println(FourByFour);
+    System.out.println(FourByFour.addQueen(2,2) + " should be false");
+    System.out.println(FourByFour);
   }
 }
