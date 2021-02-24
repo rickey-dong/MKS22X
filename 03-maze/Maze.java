d3af3c1b8e1658e93d6049db7ec4402abd4cf0c2
Rickey Dong <rickeyd2004@gmail.com>
2021-02-24 13:14:22 -0500

    terminal animation commands

--- 03-maze/Maze.java
+++ 03-maze/Maze.java
@@ -4,4 +4,26 @@ public class Maze
 {
   private char[][] maze;
   private boolean animate;
+  private void wait(int millis)
+  {
+    try 
+    {
+      Thread.sleep(millis);
+    }
+    catch(InterruptedException e) 
+    {
+    }
+  }
+  public void setAnimate(boolean b)
+  {
+    animate = b;
+  }
+  public static void clearTerminal()
+  {
+    System.out.println("\033[2J");
+  }
+  public static void gotoTop()
+  {
+    System.out.println("\033[1;1H");
+  }
 }
