9df6d493014a3341669121bc811b0070943c4728
Rickey Dong <rickeyd2004@gmail.com>
2021-02-27 17:57:10 -0500

    set animation functions to the side

--- 03-maze/Maze.java
+++ 03-maze/Maze.java
@@ -29,33 +29,33 @@ public class Maze
     }
     animate = false;
   }
-  private void wait(int millis)
-  {
-    try 
-    {
-      Thread.sleep(millis);
-    }
-    catch(InterruptedException e) 
-    {
-    }
-  }
-  public void setAnimate(boolean b)
-  {
-    animate = b;
-  }
-  public static void clearTerminal()
-  {
-    System.out.println("\033[2J");
-  }
-  public static void gotoTop()
-  {
-    System.out.println("\033[1;1H");
-  }
-  private static String colorize(String s)
-  {
-    s = s.replace("@", "\033[32m\033[49m@\033[0m");
-    s = s.replace("#", "\033[37m\033[47m#\033[0m");
-    s = s.replace("E", "\033[35m\033[49mE\033[0m");
-    return s;
-  }
+                        private void wait(int millis)
+                        {
+                          try 
+                          {
+                            Thread.sleep(millis);
+                          }
+                          catch(InterruptedException e) 
+                          {
+                          }
+                        }
+                        public void setAnimate(boolean b)
+                        {
+                          animate = b;
+                        }
+                        public static void clearTerminal()
+                        {
+                          System.out.println("\033[2J");
+                        }
+                        public static void gotoTop()
+                        {
+                          System.out.println("\033[1;1H");
+                        }
+                        private static String colorize(String s)
+                        {
+                          s = s.replace("@", "\033[32m\033[49m@\033[0m");
+                          s = s.replace("#", "\033[37m\033[47m#\033[0m");
+                          s = s.replace("E", "\033[35m\033[49mE\033[0m");
+                          return s;
+                        }
 }
