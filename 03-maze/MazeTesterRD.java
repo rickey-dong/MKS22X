41847e9c8d6ce4632438e53128a9a9724c431a97
Rickey Dong <rickeyd2004@gmail.com>
2021-02-28 11:11:22 -0500

    completed maze solver lab

--- 03-maze/MazeTesterRD.java
+++ 03-maze/MazeTesterRD.java
@@ -5,7 +5,7 @@ public class MazeTesterRD
   {
     try
     {
-      Maze easyMaze = new Maze("data3.dat");
+      Maze easyMaze = new Maze("data1.dat");
       System.out.println(easyMaze);
       easyMaze.setAnimate(true);
       System.out.println(easyMaze.solve() + " steps");
