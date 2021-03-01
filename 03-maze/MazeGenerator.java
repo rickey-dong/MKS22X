ff3bd066c074d193c171eabe77881c4e1e425aa7
Rickey Dong <rickeyd2004@gmail.com>
2021-03-01 15:48:13 -0500

    countAdjacency function

--- 03-maze/MazeGenerator.java
+++ 03-maze/MazeGenerator.java
@@ -1,7 +1,29 @@
 import java.util.*;
 public class MazeGenerator
 {
-  private char[][] maze;
-  private ArrayList<Integer> moves;
-  
+  public static void generate(char[][] maze, int rows, int cols, int startRow, int startCol)
+  {
+    
+  }
+  public static int countAdjacency(char[][] maze, int currentRow, int currentCol)
+  {
+    int count = 0;
+    if (maze[currentRow][currentCol+1] == ' ')
+    {
+      count++;
+    }
+    if (maze[currentRow][currentCol-1] == ' ')
+    {
+      count++;
+    }
+    if (maze[currentRow+1][currentCol] == ' ')
+    {
+      count++;
+    }
+    if (maze[currentRow-1][currentCol] == ' ')
+    {
+      count++;
+    }
+    return count;
+  }
 }
