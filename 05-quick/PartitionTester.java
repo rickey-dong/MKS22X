28aca5e965903b4272ac6656041f64fcdd757636
Rickey Dong <rickeyd2004@gmail.com>
2021-03-09 21:52:19 -0500

    more testing

--- 05-quick/PartitionTester.java
+++ 05-quick/PartitionTester.java
@@ -3,9 +3,9 @@ public class PartitionTester
 {
   public static void main(String[] args)
   {
-    int[] easyToLookAt = {1, 3, 5, 2, 7, 4};
+    int[] easyToLookAt = {1, 3, 5};
     int i = 0;
-    System.out.println(Preliminary.partition(easyToLookAt, 4, 5) + " is the index of the pivot");
+    System.out.println(Preliminary.partition(easyToLookAt, 0, 1) + " is the index of the pivot");
     System.out.println(Arrays.toString(easyToLookAt));
     //indices used:     3 ,     4,  5,6 ,7
   }
