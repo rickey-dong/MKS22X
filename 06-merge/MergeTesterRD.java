52d0706f89c6595981d7663a27ee84270e68f38c
Rickey Dong <rickeyd2004@gmail.com>
2021-03-16 15:43:22 -0400

    testing with sorted, reverse sorted, one value, random, binary

--- 06-merge/MergeTesterRD.java
+++ 06-merge/MergeTesterRD.java
@@ -1,11 +1,66 @@
 import java.util.Arrays;
+import java.util.Random;
 public class MergeTesterRD
 {
   public static void main(String[] args)
   {
-    int[] messyArray = {5,7,6,3,2,1,6,7,8,9,19,34,22,5,66,7,7,3,3,22,2,7};
-    System.out.println(Arrays.toString(messyArray));
-    Merge.mergesort(messyArray);
-    System.out.println(Arrays.toString(messyArray));
+    Random r = new Random();
+    int[] sorted = new int[1000000];
+    for (int i = 0; i < sorted.length; i++)
+    {
+      sorted[i] = i;
+    }
+    Merge.mergesort(sorted);
+    System.out.println(isSorted(sorted));
+    System.out.println("--------------------------------------------");
+    int[] reverseSorted = new int[1000000];
+    for (int i = 0; i < reverseSorted.length; i++)
+    {
+      reverseSorted[i] = reverseSorted.length - i;
+    }
+    Merge.mergesort(reverseSorted);
+    System.out.println(isSorted(sorted));
+    System.out.println("--------------------------------------------");
+    int[] oneValue = new int[1000000];
+    for (int i = 0; i < oneValue.length; i++)
+    {
+      oneValue[i] = 66;
+    }
+    Merge.mergesort(oneValue);
+    System.out.println(isSorted(oneValue));
+    System.out.println("--------------------------------------------");
+    int[] randomArray = new int[1000000];
+    for (int i = 0; i < randomArray.length; i++)
+    {
+      randomArray[i] = r.nextInt(100);
+    }
+    Merge.mergesort(randomArray);
+    System.out.println(isSorted(randomArray));
+    System.out.println("--------------------------------------------");
+    int[] binaryArray = new int[1000000];
+    for (int i = 0; i < binaryArray.length; i++)
+    {
+      if (i % 2 == 0)
+      {
+        binaryArray[i] = 0;
+      }
+      else
+      {
+        binaryArray[i] = 1;
+      }
+    }
+    Merge.mergesort(binaryArray);
+    System.out.println(isSorted(binaryArray));
+  }
+  public static boolean isSorted(int[] data)
+  {
+    for (int i = 1; i < data.length; i++)
+    {
+      if (data[i-1] > data[i])
+      {
+        return false;
+      }
+    }
+    return true;
   }
 }
