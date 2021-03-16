0f847f87ecdd432b61d81c527caa2cd54666ed3f
Rickey Dong <rickeyd2004@gmail.com>
2021-03-16 15:26:45 -0400

    more testing

--- 06-merge/MergeTesterRD.java
+++ 06-merge/MergeTesterRD.java
@@ -3,7 +3,7 @@ public class MergeTesterRD
 {
   public static void main(String[] args)
   {
-    int[] messyArray = {5,7,2};
+    int[] messyArray = {5,7,6,3,2,1,6,7,8,9,19,34,22,5,66,7,7,3,3,22,2,7};
     System.out.println(Arrays.toString(messyArray));
     Merge.mergesort(messyArray);
     System.out.println(Arrays.toString(messyArray));
