0588c66a1d97b3ef0657683ed787650d17d407bf
Rickey Dong <rickeyd2004@gmail.com>
2021-03-16 13:01:20 -0400

    mergesort method

--- 06-merge/Merge.java
+++ 06-merge/Merge.java
@@ -2,6 +2,15 @@ public class Merge
 {
   public static void mergesort(int[] data)
   {
-    mergesort(data, lo, hi);
+    mergesort(data, 0, data.length-1);
+  }
+  public static void mergesort(int[] data, int lo, int hi)
+  {
+    if (lo < hi)
+    {
+      mergesort(data, lo, (lo+hi)/2);
+      mergesort(data, ((lo+hi)/2)+1, hi);
+      merge(data, lo, (lo+hi)/2, ((lo+hi)/2+1), hi);
+    }
   }
 }
