7b87d57fbbd6f0ea51c421950b7e70c936cf1709
Rickey Dong <rickeyd2004@gmail.com>
2021-03-20 19:10:33 -0400

    constructor with supression warning

--- 07-mydeque/MyDeque.java
+++ 07-mydeque/MyDeque.java
@@ -2,4 +2,10 @@ public class MyDeque<E>
 {
   private E[] data;
   private int size, start, end;
+  public MyDeque()
+  {
+    @SuppressWarnings("unchecked")
+    E[] d = (E[])new Object[10];
+    data = d;
+  }
 }
