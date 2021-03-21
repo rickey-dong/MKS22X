861f53b4f1fe246207612e20a4e0e664ebb6ca77
Rickey Dong <rickeyd2004@gmail.com>
2021-03-20 20:21:02 -0400

    added size, start, end to constructors

--- 07-mydeque/MyDeque.java
+++ 07-mydeque/MyDeque.java
@@ -7,12 +7,21 @@ public class MyDeque<E>
     @SuppressWarnings("unchecked")
     E[] d = (E[])new Object[10];
     data = d;
+    size = 0;
+    start = 0;
+    end = data.length - 1;
   }
   public MyDeque(int initialCapacity)
   {
     @SuppressWarnings("unchecked")
     E[] d = (E[])new Object[initialCapacity];
     data = d;
+    start = 0;
+    end = data.length - 1;
+    if (initialCapacity == 0)
+    {
+      end = 0;
+    }
   }
   public int size()
   {
