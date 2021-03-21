48da16fa4e56fdb1d1b4baad8d94e58de6271ef2
Rickey Dong <rickeyd2004@gmail.com>
2021-03-21 14:19:13 -0400

    added comments

--- 07-mydeque/MyDeque.java
+++ 07-mydeque/MyDeque.java
@@ -1,106 +1,106 @@
 import java.util.NoSuchElementException;
 public class MyDeque<E>
 {
-  private E[] data;
-  private int size, start, end;
+  private E[] data; //this is an ArrayDeque
+  private int size, start, end; //two pointers for head and tail and keep track of current size
   public MyDeque()
   {
     @SuppressWarnings("unchecked")
-    E[] d = (E[])new Object[10];
+    E[] d = (E[])new Object[10]; //default capacity 10
     data = d;
-    size = 0;
-    start = 0;
-    end = data.length - 1;
+    size = 0; //there are no elements in there so far
+    start = 0; //start is 0
+    end = 9; //end is 9
   }
-  public MyDeque(int initialCapacity)
+  public MyDeque(int initialCapacity) //user chooses capacity
   {
     @SuppressWarnings("unchecked")
     E[] d = (E[])new Object[initialCapacity];
     data = d;
-    start = 0;
-    end = data.length - 1;
-    if (initialCapacity == 0)
+    start = 0; //start is 0
+    end = data.length - 1; //end is the last index
+    if (initialCapacity == 0) //unless the capacity is 0, in which case make them -1
     {
       end = -1;
       start = -1;
     }
-    size = 0;
+    size = 0; //currently no elements in there so far
   }
   public int size()
   {
-    return size;
+    return size; //return size
   }
   public void addFirst(E element)
   {
     if (element == null)
     {
-      throw new NullPointerException("this deque does not permit null elements");
+      throw new NullPointerException("this deque does not permit null elements"); //cannot be null
     }
-    if (size == data.length)
+    if (size == data.length) //we have no more space
     {
       resize();
     }
-    data[start] = element;
-    start++;
-    size++;
+    data[start] = element; //insert the element
+    start++; //start moves one index to the right 
+    size++; //size increases by 1
   }
   public void addLast(E element)
   {
     if (element == null)
     {
-      throw new NullPointerException("this deque does not permit null elements");
+      throw new NullPointerException("this deque does not permit null elements"); //cannot be null
     }
-    if (size == data.length)
+    if (size == data.length) //we have no more space
     {
       resize();
     }
-    data[end] = element;
-    end--;
-    size++;
+    data[end] = element; //insert the element
+    end--; //end moves one index to the left
+    size++; //size increases by 1
   }
   private void resize()
   {
     @SuppressWarnings("unchecked")
-    E[] largerDeque = (E[])new Object[(data.length * 2) + 1];
-    if (data.length == 0)
+    E[] largerDeque = (E[])new Object[(data.length * 2) + 1]; //make a larger deque
+    if (data.length == 0) //math manip to prevent indexing errors with -1
     {
       start = 0;
       end = 0;
     }
     for (int i = 0; i <= start-1; i++)
     {
-      largerDeque[i] = data[i];
+      largerDeque[i] = data[i]; //transplant the elements from the beginning into the larger deque
     }
     int oldLength = data.length-1;
-    int newEnding = largerDeque.length - 1;
+    int newEnding = largerDeque.length - 1; //math manip to set up the transferring of the elements from the end
     for (int i = largerDeque.length - 1; oldLength > end; oldLength--)
     {
       largerDeque[i] = data[oldLength];
       i--;
-      newEnding = i;
+      newEnding = i; //transplant the elements from the end into the larger deque
     }
-    end = newEnding;
-    data = largerDeque;
+    end = newEnding; //new pointer for end in the larger deque but start stays the same
+    data = largerDeque; //we have a new larger deque
   }
   public E removeFirst()
   {
     if (size == 0)
     {
-      throw new NoSuchElementException("this deque is empty");
+      throw new NoSuchElementException("this deque is empty"); //can't remove thin air
     }
     E removedThis;
     if (start != 0)
     {
-      removedThis = data[start-1];
+      removedThis = data[start-1]; //remove the element that was one index behind start and move start down
       start--;
       size--;
     }
-    else
+    else //this means you didn't add anything from the start, but you did add things from the back
     {
-      removedThis = data[data.length-end];
+      removedThis = data[data.length-end]; // the last element is the head when you only added from the back
       size--;
       end++;
-    }
+    }//size decreases and pointer moves around
     return removedThis;
   }
   public E removeLast()
@@ -116,7 +116,7 @@ public class MyDeque<E>
       end++;
       size--;
     }
-    else
+    else //this means you only added from start and not from end
     {
       removedThis = data[Math.abs(size-start)];
       size--;
@@ -129,7 +129,7 @@ public class MyDeque<E>
     {
       throw new NoSuchElementException("this deque is empty");
     }
-    if (start == 0)
+    if (start == 0) //this means we only added from back
     {
       return data[data.length-1];
     }
@@ -141,7 +141,7 @@ public class MyDeque<E>
     {
       throw new NoSuchElementException("this deque is empty");
     }
-    if (end == data.length - 1)
+    if (end == data.length - 1) //this means we only added from front
     {
       return data[0];
     }
