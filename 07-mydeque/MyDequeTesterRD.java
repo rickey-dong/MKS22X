c7b0681ec9596c49d3a7253b94905ea012b16ead
Rickey Dong <rickeyd2004@gmail.com>
2021-03-21 00:18:52 -0400

    size testing

--- 07-mydeque/MyDequeTesterRD.java
+++ 07-mydeque/MyDequeTesterRD.java
@@ -4,6 +4,7 @@ public class MyDequeTesterRD
   public static void main(String[] args)
   {
     MyDeque<Integer> numbers = new MyDeque<Integer>(4);
+    System.out.println(numbers.size() + " should be 0");
     System.out.println(numbers.toString() + " should be {}");
     try{
       System.out.println(numbers.getFirst());
@@ -52,11 +53,13 @@ public class MyDequeTesterRD
     numbers.addLast(559);
     System.out.println(numbers.getLast() + " should be 559");
     System.out.println(numbers.toString() + " should be {10, 6, 8, 559}"); //[6,10, 559, 8] start = 2 , end = 1
+    System.out.println(numbers.size() + " should be 4");
     //System.out.println(numbers.removeFirst() + " should be 10"); //[6, 10 559, 8] start = 2 end = 2 size = 3
     //System.out.println(numbers.removeFirst() + " should be 6"); // start = 2 end = 3 size = 2
     System.out.println(numbers.removeLast() + " should be 559"); // start = 0 end = 2 size = 1
     System.out.println(numbers.removeLast() + " should be 8"); //start = 0 end = 3 size = 0
     System.out.println(numbers.removeLast() + " should be 6");
+    System.out.println(numbers.size() + " should be 1");
     System.out.println(numbers.removeLast() + " should be 10");
     try{
       System.out.println(numbers.removeLast());
