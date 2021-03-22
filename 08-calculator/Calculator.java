4980b9a2516d3c34d5d6c45a03bcd03d2665288e
Rickey Dong <rickeyd2004@gmail.com>
2021-03-22 13:03:48 -0400

    parsing the input works

--- 08-calculator/Calculator.java
+++ 08-calculator/Calculator.java
@@ -1,8 +1,15 @@
 import java.util.ArrayDeque;
+import java.util.Arrays;
 public class Calculator
 {
-  public static double eval(String s)
+  public static void eval(String s)
   {
-    
+    ArrayDeque<Double> calcStack = new ArrayDeque<Double>();
+    String[] operatorsAndOperands = s.split(" ");
+    for (String o : operatorsAndOperands)
+    {
+      System.out.println(o);
+    }
+    System.out.println(Arrays.toString(operatorsAndOperands));
   }
 }
---
+++ 08-calculator/CalculatorTesterRD.java
@@ -0,0 +1,7 @@
+public class CalculatorTesterRD
+{
+  public static void main(String[] args)
+  {
+    Calculator.eval("1 2 3 4 5 + * - -");
+  }
+}
