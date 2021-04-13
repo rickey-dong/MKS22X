3d167e0e5012fcbaa5e07b50e0635ee482d197ec
Rickey Dong <rickeyd2004@gmail.com>
2021-04-13 12:22:51 -0400

    frontier done?

--- 09-treeburning/Frontier.java
+++ 09-treeburning/Frontier.java
@@ -10,4 +10,12 @@ public class Frontier
   {
     return frontier.size();
   }
+  public void add(int[] location)
+  {
+    frontier.add(location);
+  }
+  public int[] remove()
+  {
+    return frontier.remove();
+  }
 }
