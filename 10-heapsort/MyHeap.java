64b4430564ab7ad8ca6aa57d7db09e10b6de8621
Rickey Dong <rickeyd2004@gmail.com>
2021-04-23 02:40:54 -0400

    editing booleans

--- 10-heapsort/MyHeap.java
+++ 10-heapsort/MyHeap.java
@@ -2,24 +2,31 @@ public class MyHeap
 {
   public static void pushDown(int[] data, int size, int index)
   {
-    if (index < size/2 //if it is NOT a leaf (b/c 50% of tree are leaves) AND
+    if  ( (index * 2) + 1 < size //if it is NOT a leaf (b/c you have at least one child) AND
         && (data[index] < data[(index * 2) + 1] //not bigger than left child OR
-        || data[index] < data[(index * 2) + 2]))//not bigger than right child
+        || ((index * 2) + 2 < size && data[index] < data[(index * 2) + 2]) ) )//not bigger than right child
     {
       int leftChild = data[(index * 2) + 1];
-      int rightChild = data[(index * 2) + 2];
-      if (leftChild <= rightChild)
+      if ( (index * 2) + 2 < size)
       {
-        int swap = data[index];
-        data[index] = data[(index * 2) + 2];
-        data[(index * 2) + 2] = swap;
-        pushDown(data, size, (index * 2) + 2);
+        int rightChild = data[(index * 2) + 2];
+        if (leftChild <= rightChild)
+        {
+          int swap = data[index];
+          data[index] = data[(index * 2) + 2];
+          data[(index * 2) + 2] = swap;
+          pushDown(data, size, (index * 2) + 2);
+        }
+        else
+        {
+          int swap = data[index];
+          data[index] = data[(index * 2) + 1];
+          data[(index * 2) + 1] = swap;
+          pushDown(data, size, (index * 2) + 1);
+        }
       }
       else
       {
-        int swap = data[index];
-        data[index] = data[(index * 2) + 1];
-        data[(index * 2) + 1] = swap;
         pushDown(data, size, (index * 2) + 1);
       }
     }
