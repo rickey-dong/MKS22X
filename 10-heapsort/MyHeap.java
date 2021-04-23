e12be3abf64af1e1d79dabe311502df16969f583
Rickey Dong <rickeyd2004@gmail.com>
2021-04-23 01:36:59 -0400

    pushDown method

--- 10-heapsort/MyHeap.java
+++ 10-heapsort/MyHeap.java
@@ -2,13 +2,26 @@ public class MyHeap
 {
   private static void pushDown(int[] data, int size, int index)
   {
-    
-  }
-  private static int[] getIndicesOfChildren(int parentIndex)
-  {
-    int[] childrenIndices = new int[2];
-    childrenIndices[0] = (parentIndex * 2) + 1;
-    childrenIndices[1] = (parentIndex * 2) + 2;
-    return childrenIndices;
+    if (index < size/2 //if it is NOT a leaf (b/c 50% of tree are leaves) AND
+        && (data[index] < data[(index * 2) + 1] //not bigger than left child OR
+        || data[index] < data[(index * 2) + 2]))//not bigger than right child
+    {
+      int leftChild = data[(index * 2) + 1];
+      int rightChild = data[(index * 2) + 2];
+      if (leftChild <= rightChild)
+      {
+        int swap = data[index];
+        data[index] = data[(index * 2) + 2];
+        data[(index * 2) + 2] = swap;
+        pushDown(data, size, (index * 2) + 2);
+      }
+      else
+      {
+        int swap = data[index];
+        data[index] = data[(index * 2) + 1];
+        data[(index * 2) + 1] = swap;
+        pushDown(data, size, (index * 2) + 1);
+      }
+    }
   }
 }
