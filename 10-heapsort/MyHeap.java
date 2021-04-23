352d14c2c931549b9baae26c2570affd48defbf7
Rickey Dong <rickeyd2004@gmail.com>
2021-04-23 01:17:13 -0400

    getIndicesOfChildren private method

--- 10-heapsort/MyHeap.java
+++ 10-heapsort/MyHeap.java
@@ -4,4 +4,11 @@ public class MyHeap
   {
     
   }
+  private static int[] getIndicesOfChildren(int parentIndex)
+  {
+    int[] childrenIndices = new int[2];
+    childrenIndices[0] = (parentIndex * 2) + 1;
+    childrenIndices[1] = (parentIndex * 2) + 2;
+    return childrenIndices;
+  }
 }
