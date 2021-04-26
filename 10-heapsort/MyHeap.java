6a063ab0b612f245aa249c1a6b0ca192368d390b
Rickey Dong <rickeyd2004@gmail.com>
2021-04-26 18:11:20 -0400

    implemented heapsort and fixed pushDown so that it better ccounted for the size decreasing and the right child existence checking

--- 10-heapsort/MyHeap.java
+++ 10-heapsort/MyHeap.java
@@ -4,9 +4,41 @@ public class MyHeap
   {
     if ((index * 2) + 1 < size) //if it is NOT a leaf b/c you have at least one child, the left one
     {
-      try
+      // try
+      // {
+      //   if (data[index] < data[(index * 2) + 2] || data[index] < data[(index * 2) + 1]) //if smaller than right or left child, if right exists
+      //   {
+      //     int leftChild = data[(index * 2) + 1];
+      //     int rightChild = data[(index * 2) + 2];
+      //     if (leftChild <= rightChild)
+      //     {
+      //       int swap = data[index];
+      //       data[index] = data[(index * 2) + 2];
+      //       data[(index * 2) + 2] = swap;
+      //       pushDown(data, size, (index * 2) + 2);
+      //     }
+      //     else
+      //     {
+      //       int swap = data[index];
+      //       data[index] = data[(index * 2) + 1];
+      //       data[(index * 2) + 1] = swap;
+      //       pushDown(data, size, (index * 2) + 1);
+      //     }
+      //   }
+      // }
+      // catch(ArrayIndexOutOfBoundsException e) //right child does not exist
+      // {
+      //   if (data[index] < data[(index * 2) + 1]) //if smaller than left child
+      //   {
+      //     int swap = data[index];
+      //     data[index] = data[(index * 2) + 1];
+      //     data[(index * 2) + 1] = swap;
+      //     pushDown(data, size, (index * 2) + 1);
+      //   }
+      // }
+      if ((index * 2) + 2 < size) //if right child exists
       {
-        if (data[index] < data[(index * 2) + 2] || data[index] < data[(index * 2) + 1]) //if smaller than right or left child, if right exists
+        if (data[index] < data[(index * 2) + 2] || data[index] < data[(index * 2) + 1]) //if smaller than right or left child
         {
           int leftChild = data[(index * 2) + 1];
           int rightChild = data[(index * 2) + 2];
@@ -26,7 +58,7 @@ public class MyHeap
           }
         }
       }
-      catch(ArrayIndexOutOfBoundsException e) //right child does not exist
+      else //right child does not exist
       {
         if (data[index] < data[(index * 2) + 1]) //if smaller than left child
         {
@@ -51,6 +83,14 @@ public class MyHeap
     data[0] = data[size-1];
     data[size-1] = swap;
     pushDown(data, size-1, 0);
+  }
+  public static void heapsort(int[] data)
+  {
+    buildHeap(data);
+    for (int heapSize = data.length; heapSize > 0; heapSize--)
+    {
+      remove(data, heapSize);
+    }
   }
 }
 
