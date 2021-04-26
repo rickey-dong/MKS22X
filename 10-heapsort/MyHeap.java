5d246ce2680afaf509270745d14ed167582db1e8
Rickey Dong <rickeyd2004@gmail.com>
2021-04-26 17:10:03 -0400

    replaced old unruly code with trycatch block for index out of bounds for right child checking

--- 10-heapsort/MyHeap.java
+++ 10-heapsort/MyHeap.java
@@ -2,35 +2,39 @@ public class MyHeap
 {
   public static void pushDown(int[] data, int size, int index)
   {
-    if  ( (index * 2) + 1 < size //if it is NOT a leaf (b/c you have at least one child) AND
-        && (data[index] < data[(index * 2) + 1] //not bigger than left child OR
-        || ((index * 2) + 2 < size && data[index] < data[(index * 2) + 2]) ) )//not bigger than right child
+    if ((index * 2) + 1 < size) //if it is NOT a leaf b/c you have at least one child, the left one
     {
-      int leftChild = data[(index * 2) + 1];
-      if ( (index * 2) + 2 < size)
+      try
       {
-        int rightChild = data[(index * 2) + 2];
-        if (leftChild <= rightChild)
+        if (data[index] < data[(index * 2) + 2] || data[index] < data[(index * 2) + 1]) //if smaller than right or left child, if right exists
         {
-          int swap = data[index];
-          data[index] = data[(index * 2) + 2];
-          data[(index * 2) + 2] = swap;
-          pushDown(data, size, (index * 2) + 2);
+          int leftChild = data[(index * 2) + 1];
+          int rightChild = data[(index * 2) + 2];
+          if (leftChild <= rightChild)
+          {
+            int swap = data[index];
+            data[index] = data[(index * 2) + 2];
+            data[(index * 2) + 2] = swap;
+            pushDown(data, size, (index * 2) + 2);
+          }
+          else
+          {
+            int swap = data[index];
+            data[index] = data[(index * 2) + 1];
+            data[(index * 2) + 1] = swap;
+            pushDown(data, size, (index * 2) + 1);
+          }
         }
-        else
+      }
+      catch(ArrayIndexOutOfBoundsException e) //right child does not exist
+      {
+        if (data[index] < data[(index * 2) + 1]) //if smaller than left child
         {
           int swap = data[index];
           data[index] = data[(index * 2) + 1];
           data[(index * 2) + 1] = swap;
           pushDown(data, size, (index * 2) + 1);
         }
-      }
-      else
-      {
-        int swap = data[index];
-        data[index] = data[(index * 2) + 1];
-        data[(index * 2) + 1] = swap;
-        pushDown(data, size, (index * 2) + 1);
       }
     }
   }
