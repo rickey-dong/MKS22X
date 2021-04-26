3648f28e6b525941d85ff10571d3f7564c4a3a2a
Rickey Dong <rickeyd2004@gmail.com>
2021-04-26 18:20:28 -0400

    removed comments

--- 10-heapsort/MyHeap.java
+++ 10-heapsort/MyHeap.java
@@ -1,41 +1,9 @@
 public class MyHeap
 {
-  private static void pushDown(int[] data, int size, int index)
+  public static void pushDown(int[] data, int size, int index)
   {
     if ((index * 2) + 1 < size) //if it is NOT a leaf b/c you have at least one child, the left one
     {
-      // try
-      // {
-      //   if (data[index] < data[(index * 2) + 2] || data[index] < data[(index * 2) + 1]) //if smaller than right or left child, if right exists
-      //   {
-      //     int leftChild = data[(index * 2) + 1];
-      //     int rightChild = data[(index * 2) + 2];
-      //     if (leftChild <= rightChild)
-      //     {
-      //       int swap = data[index];
-      //       data[index] = data[(index * 2) + 2];
-      //       data[(index * 2) + 2] = swap;
-      //       pushDown(data, size, (index * 2) + 2);
-      //     }
-      //     else
-      //     {
-      //       int swap = data[index];
-      //       data[index] = data[(index * 2) + 1];
-      //       data[(index * 2) + 1] = swap;
-      //       pushDown(data, size, (index * 2) + 1);
-      //     }
-      //   }
-      // }
-      // catch(ArrayIndexOutOfBoundsException e) //right child does not exist
-      // {
-      //   if (data[index] < data[(index * 2) + 1]) //if smaller than left child
-      //   {
-      //     int swap = data[index];
-      //     data[index] = data[(index * 2) + 1];
-      //     data[(index * 2) + 1] = swap;
-      //     pushDown(data, size, (index * 2) + 1);
-      //   }
-      // }
       if ((index * 2) + 2 < size) //if right child exists
       {
         if (data[index] < data[(index * 2) + 2] || data[index] < data[(index * 2) + 1]) //if smaller than right or left child
@@ -70,7 +38,7 @@ public class MyHeap
       }
     }
   }
-  private static void buildHeap(int[] data)
+  public static void buildHeap(int[] data)
   {
     for (int currentIndex = data.length-1; currentIndex >= 0; currentIndex--)
     {
